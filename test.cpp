6996b8d172ef714f646e1c9e1d81abe043a1c89d
June <juneee0864@gmail.com>
2021-10-08 10:53:29 +0900

    9단까지 구구

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(){
-    for(int i=1;i<9;i++){
-        for(int j=1;j<=9;j++){
+    for(int i=1;i<10;i++){
+        for(int j=1;j<=10;j++){
             std::cout<<i<<"*"<<j<<"="<<i*j<<std::endl;
         }
     }
